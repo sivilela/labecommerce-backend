6b89d2ba077d0405381888ffaa17ce720d0ffc47
sivilela <sivilela.s@gmail.com>
2024-01-09 20:29:23 -0300

    Realizado os exercícios de Relações em  SQL I

--- src/database/labecommerce.sql
+++ src/database/labecommerce.sql
@@ -68,3 +68,53 @@ SET
 WHERE id = 'p002';
 
   DROP TABLE products;
+
+
+  CREATE TABLE purchases (
+    id TEXT PRIMARY KEY UNIQUE NOT NULL,
+    buyer TEXT NOT NULL,
+    total_price REAL NOT NULL,
+    created_at TEXT NOT NULL,
+    FOREIGN KEY (buyer) REFERENCES users (id)
+  );
+
+INSERT INTO purchases (id, buyer, total_price, created_at)
+VALUES
+  ('c0010', 'u002', 100.00, '09/01/2024'),
+  ('c0011', 'u003', 100.00, '09/01/2024'),
+  ('c0012', 'u004', 100.00, '09/01/2024'),
+  ('c0013', 'u002', 500.00, '09/01/2024');
+
+  DROP TABLE purchases
+
+  INSERT INTO purchases (id, buyer, total_price, created_at)
+VALUES
+  ('c002', 'u001', 100.00, '09/01/2024');
+ 
+
+  SELECT * FROM purchases
+
+UPDATE purchases
+SET
+  total_price = 199,
+WHERE id = 'c001';
+
+SELECT
+  purchases.id,
+  users.id,
+  users.name,
+  users.email,
+  purchases.total_price,
+  purchases.created_at
+FROM
+  purchases
+  INNER JOIN users ON users.id = purchases.buyer;
+
+  -- teste INNER
+  SELECT
+    purchases.total_price,
+    users.name
+  FROM
+    purchases
+    INNER JOIN users on purchases.buyer = users.id;
+
